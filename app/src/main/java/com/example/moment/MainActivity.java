5bdbc70183143ddc2c79aaa6233b41a924b3044b
dain <ekdls1219@gmail.com>
2021-06-01 21:34:51 +0900

    tmap and recordlist

--- app/src/main/java/com/example/moment/MainActivity.java
+++ app/src/main/java/com/example/moment/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
         // loading 화면
         Intent intent = new Intent(this, LoadingActivity.class);
         startActivity(intent);
+
+        // '기록시작' 버튼 클릭
+        Button btn_start = (Button) findViewById(R.id.button1);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // '기록보기' 버튼 클릭
+        Button btn_record = (Button) findViewById(R.id.button2);
+        btn_record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RecordlistActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/moment/MapActivity.java
@@ -0,0 +1,67 @@
+package com.example.moment;
+
+import android.Manifest;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.skt.Tmap.TMapGpsManager;
+import com.skt.Tmap.TMapView;
+
+public class MapActivity extends AppCompatActivity implements TMapGpsManager.onLocationChangedCallback {
+
+    String API_Key = "API Key 입력";
+
+    // T Map View
+    TMapView tMapView = null;
+
+    // T Map GPS
+    TMapGpsManager tMapGPS = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        // T Map View
+        tMapView = new TMapView(this);
+
+        // API Key
+        tMapView.setSKTMapApiKey(API_Key);
+
+        // Initial Setting
+        tMapView.setZoomLevel(17);
+        tMapView.setIconVisibility(true);
+        tMapView.setMapType(TMapView.MAPTYPE_STANDARD);
+        tMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+
+        // T Map View Using Linear Layout
+        LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
+        linearLayoutTmap.addView(tMapView);
+
+        // Request For GPS permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+        // GPS using T Map
+        tMapGPS = new TMapGpsManager(this);
+
+        // Initial Setting
+        tMapGPS.setMinTime(1000);
+        tMapGPS.setMinDistance(10);
+        tMapGPS.setProvider(TMapGpsManager.NETWORK_PROVIDER);
+        //tMapGPS.setProvider(tMapGPS.GPS_PROVIDER);
+
+        tMapGPS.OpenGps();
+    }
+
+    @Override
+    public void onLocationChange(Location location) {
+        tMapView.setLocationPoint(location.getLongitude(), location.getLatitude());
+        tMapView.setCenterPoint(location.getLongitude(), location.getLatitude());
+    }
+}
