8833d1fd2811f7d2020d4098a9bd1d420373d5bc
uno-pj <uno_kim@naver.com>
2021-06-03 16:25:04 +0900

    Empty

--- app/src/main/java/com/example/moment/MapActivity.java
+++ app/src/main/java/com/example/moment/MapActivity.java
@@ -1,19 +1,32 @@
 package com.example.moment;
 
 import android.Manifest;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.skt.Tmap.TMapGpsManager;
+import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapPolyLine;
 import com.skt.Tmap.TMapView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class MapActivity extends AppCompatActivity implements TMapGpsManager.onLocationChangedCallback {
 
-    String API_Key = "API Key 입력";
+    String API_Key = "l7xx4e73edbc96c742a0a4a0f8379a11d274";
 
     // T Map View
     TMapView tMapView = null;
@@ -25,6 +38,17 @@ public class MapActivity extends AppCompatActivity implements TMapGpsManager.onL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        Button stopRec = findViewById(R.id.stopRec);
+        //button setting
+        stopRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tMapGPS.CloseGps();
+                Toast.makeText(getApplicationContext(),"경로 기록을 종료합니다.", Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         // T Map View
         tMapView = new TMapView(this);
@@ -54,14 +78,69 @@ public class MapActivity extends AppCompatActivity implements TMapGpsManager.onL
         tMapGPS.setMinTime(1000);
         tMapGPS.setMinDistance(10);
         tMapGPS.setProvider(TMapGpsManager.NETWORK_PROVIDER);
-        //tMapGPS.setProvider(tMapGPS.GPS_PROVIDER);
 
+        //tMapGPS.setProvider(tMapGPS.GPS_PROVIDER);
         tMapGPS.OpenGps();
+
+        //스크린캡쳐 버튼 할당
+        Button share = (Button) findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage();
+                Toast.makeText(getApplicationContext(),"캡쳐했습니다.", Toast.LENGTH_SHORT).show();}
+        });
+
     }
+    //스크린캡쳐
+    public void captureImage() {
+        tMapView.getCaptureImage(20, new TMapView.MapCaptureImageListenerCallback() {
+
+            @Override
+            public void onMapCaptureImage(Bitmap bitmap) {
+
+                String sdcard = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+                File path = new File(sdcard); //+ File.separator + "image_write"
+                if (!path.exists())
+                    path.mkdir();
+
+                File fileCacheItem = new File(path.toString() + File.separator + System.currentTimeMillis() + ".png");
+                OutputStream out = null;
+
+                try {
+                    fileCacheItem.createNewFile();
+                    out = new FileOutputStream(fileCacheItem);
+
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+
+                    out.flush();
+                    out.close();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    TMapPolyLine tMapPolyLine = new TMapPolyLine();
+    ArrayList<TMapPoint> alTMapPoint = new ArrayList<TMapPoint>();
 
     @Override
     public void onLocationChange(Location location) {
         tMapView.setLocationPoint(location.getLongitude(), location.getLatitude());
         tMapView.setCenterPoint(location.getLongitude(), location.getLatitude());
+        TMapPoint point = tMapGPS.getLocation();
+        double Latitude = point.getLatitude();
+        double Longitude = point.getLongitude();
+        // Log.i("point", String.valueOf(point));
+
+        alTMapPoint.add( new TMapPoint(Latitude, Longitude) );
+        tMapPolyLine.setLineColor(Color.BLUE);
+        tMapPolyLine.setLineWidth(2);
+        tMapView.addTMapPolyLine("Line", tMapPolyLine);
     }
+
+
 }
